3ef155bc19421519a06907bf1387d1f7c54f2833
INSTIG8R <ahammedsakir99@gmail.com>
2024-03-06 01:49:25 +0600

    convolution block done

--- Modules/ResSwin.py
+++ Modules/ResSwin.py
@@ -1,9 +1,21 @@
 import torch.nn as nn
 
 from SwinTransformer import SwinTransformerBlock
+from PatchMerging import PatchMerging
 
 class ResSwin(nn.Module):
-    def __init__(self):
+    def __init__(self,input_dim,output_dim,input_resolution,num_heads,depth):
         super().__init__()
+        self.depth = depth
+        self.swin_block = SwinTransformerBlock(dim=input_dim, input_resolution=input_resolution, num_heads=num_heads)
+        self.patch_merging = PatchMerging(dim=input_dim, input_resolution=input_resolution)
         
+
+    def forward(self,x):
+        input = x
+        for i in range(self.depth):
+            block_output = self.swin_block(x)
+        input_merged = self.patch_merging(block_output)
+        
+
 
