5f05269032428ef85ed758f0f2100dd47c4d1ffb
INSTIG8R <ahammedsakir99@gmail.com>
2024-03-06 04:23:43 +0600

    attention gate done

--- temp.py
+++ temp.py
@@ -1,18 +1,18 @@
 import torch
 import torch.nn as nn
 
-x = torch.rand(2,96,3136)
-y = torch.rand(2,784,192)
+x = torch.rand(2,768,49)
+y = torch.rand(2,384,196)
 
-channel_conv = nn.Conv1d(in_channels=96,out_channels=192,kernel_size=3,padding=1)
+channel_conv = nn.Conv1d(in_channels=384,out_channels=384,kernel_size=1)
 maxpool = nn.MaxPool1d(kernel_size=4, stride=4)
 
-x_added = channel_conv(x)
+x_up = channel_conv(y)
 
-print(x_added.shape)
+print(x_up.shape)
 
-x_down = maxpool(x_added)
-print(x_down.shape)
+# x_down = maxpool(x_added)
+# print(x_down.shape)
 # x_added_2 = channel_conv(x_added)
 
 
