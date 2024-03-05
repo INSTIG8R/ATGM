1ca538dd85eb7eb6c138170dd9772f9354c4bd9e
INSTIG8R <ahammedsakir99@gmail.com>
2024-03-06 02:24:43 +0600

    res swin block done

--- Modules/ResSwin.py
+++ Modules/ResSwin.py
@@ -1,7 +1,9 @@
+import torch
 import torch.nn as nn
 
 from SwinTransformer import SwinTransformerBlock
 from PatchMerging import PatchMerging
+from Convolution import Convolution
 
 class ResSwin(nn.Module):
     def __init__(self,input_dim,output_dim,input_resolution,num_heads,depth):
@@ -9,13 +11,30 @@ class ResSwin(nn.Module):
         self.depth = depth
         self.swin_block = SwinTransformerBlock(dim=input_dim, input_resolution=input_resolution, num_heads=num_heads)
         self.patch_merging = PatchMerging(dim=input_dim, input_resolution=input_resolution)
-        
+        self.convolution = Convolution(in_channels=input_dim,out_channels=output_dim)
+        self.relu = nn.ReLU(inplace=True)
+
 
     def forward(self,x):
-        input = x
         for i in range(self.depth):
             block_output = self.swin_block(x)
-        input_merged = self.patch_merging(block_output)
+        input_patch_merged = self.patch_merging(block_output)
+        input_convolution = self.convolution(x)
+        input_joined = input_patch_merged + input_convolution
+        output = self.relu(input_joined)
+
+        return output
+    
+# if __name__=="__main__":
+#     res_swin = ResSwin(input_dim=96,output_dim=192,input_resolution=(56,56),num_heads=3,depth=2)
+#     x = torch.rand(2,3136,96)
+
+#     out = res_swin(x)
+
+#     print(out.shape) # 2,784,192
+
+
+
         
 
 
