f57102ad27d81d189dd4353ca865c9b31c56d9b1
INSTIG8R <ahammedsakir99@gmail.com>
2024-03-06 16:46:07 +0600

    model complete

---
+++ Modules/Bottleneck.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+from SwinTransformer import SwinTransformerBlock
+from Convolution import Convolution
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, input_resolution, num_heads):
+        super().__init__()
+        self.in_channels = in_channels
+        self.swin = SwinTransformerBlock(dim=in_channels, input_resolution=input_resolution, num_heads=num_heads)
+        self.conv = Convolution(in_channels=in_channels,out_channels=in_channels)
+
+    def forward(self,x,depth=2,swin=False,conv=True):
+        
+        if swin:
+            for i in range(depth):
+                x = self.swin(x)
+        if conv:
+            if x.shape[1]!=self.in_channels:
+                x = x.transpose(2,1)
+            x = self.conv(x,maxpool=False)
+            if x.shape[1]==self.in_channels:
+                x = x.transpose(2,1)
+
+        return x
+
+if __name__ == "__main__":
+    bottleneck = Bottleneck(in_channels=768, out_channels=768, input_resolution=(7,7), num_heads=24)
+    x = torch.rand(2,49,768)
+    x_bottle = bottleneck(x)
+
+    print(x.shape)
---
+++ Modules/Unet.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+from matplotlib import pyplot as plt
+
+from BERT import BERT
+from PatchEmbed import PatchEmbed
+from GuideEncoder import GuideEncoder
+from ResSwin import ResSwin
+from Bottleneck import Bottleneck
+from AttentionGate import AttentionGate
+from Deconvolution import Deconvolution
+from FinalOutput import FinalOutput
+from torchinfo import summary
+
+class Unet(nn.Module):
+    def __init__(self,num_classes):
+        super().__init__()
+        # self.BERT = BERT()
+        self.PatchEmbed = PatchEmbed()
+        self.GuideEncoder = GuideEncoder(in_channels=96, text_len=3136)
+        self.ResSwin1 = ResSwin(input_dim=96,output_dim=192,input_resolution=(56,56),num_heads=3,depth=2)
+        self.ResSwin2 = ResSwin(input_dim=192,output_dim=384,input_resolution=(28,28),num_heads=6,depth=2)
+        self.ResSwin3 = ResSwin(input_dim=384,output_dim=768,input_resolution=(14,14),num_heads=12,depth=2)
+        self.Bottleneck = Bottleneck(in_channels=768, out_channels=768, input_resolution=(7,7), num_heads=24)
+        self.AttentionGate3 = AttentionGate(gate_input_channel=768,skip_input_channel=384)
+        self.AttentionGate2 = AttentionGate(gate_input_channel=384,skip_input_channel=192)
+        self.AttentionGate1 = AttentionGate(gate_input_channel=192,skip_input_channel=96)
+        self.Decoder1 = Deconvolution(input_channel=768,skip_input_channel=384)
+        self.Decoder2 = Deconvolution(input_channel=384,skip_input_channel=192)
+        self.Decoder3 = Deconvolution(input_channel=192,skip_input_channel=96)
+        self.FinalOutput = FinalOutput(in_channel=96, out_channel=num_classes)
+
+    def forward(self,x,text):
+        text_embed = text
+        x_embed = self.PatchEmbed(x)
+        print(x_embed.shape)
+        x_guide = self.GuideEncoder(x_embed,text_embed)
+        print(x_guide.shape)
+        x_res1 = self.ResSwin1(x_guide)
+        print(x_res1.shape)
+        x_res2 = self.ResSwin2(x_res1)
+        print(x_res2.shape)
+        x_res3 = self.ResSwin3(x_res2)
+        print(x_res3.shape)
+        x_bottle = self.Bottleneck(x_res3)
+        print(x_bottle.shape)
+        skip_2 = x_res2
+        skip_1 = x_res1
+        skip_0 = x_embed
+        x_dec1 = self.Decoder1(x_res3,skip_2)
+        print(x_dec1.shape)
+        x_dec2 = self.Decoder2(x_dec1,skip_1)
+        print(x_dec2.shape)
+        x_dec3 = self.Decoder3(x_dec2,skip_0)
+        print(x_dec3.shape)
+        x_out = self.FinalOutput(x_dec3)
+        print(x_out.shape)
+
+        return x_out
+
+
+
+
+if __name__ =="__main__":
+    x = torch.rand(2,3,224,224)
+    text = torch.rand(2,10,768)
+    x, text = x.cuda(),text.cuda()
+
+    unet = Unet(num_classes=1).cuda()
+
+    # summary(unet, [(2,3,224,224),(2,10,768)])
+
+    output = unet(x,text)
+    print(output.shape)
+    output = output.transpose(1,3)
+    print(output.shape)
+
+
+    plt.imshow(output[0,0].cpu().detach().numpy())
+    plt.show()
