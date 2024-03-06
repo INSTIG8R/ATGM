b192f34b594c768085a9caea446edbec0fd07456
INSTIG8R <ahammedsakir99@gmail.com>
2024-03-06 12:42:26 +0600

    deconvolution block done

--- temp.py
+++ temp.py
@@ -1,15 +1,29 @@
 import torch
 import torch.nn as nn
 
-x = torch.rand(2,768,49)
-y = torch.rand(2,384,196)
+x = torch.rand(2,96,3136)
 
-channel_conv = nn.Conv1d(in_channels=384,out_channels=384,kernel_size=1)
-maxpool = nn.MaxPool1d(kernel_size=4, stride=4)
+channel_conv = nn.Conv1d(in_channels=96,out_channels=3,kernel_size=1)
 
-x_up = channel_conv(y)
+x_up = channel_conv(x)
 
 print(x_up.shape)
+
+x.view
+
+channel_conv = nn.ConvTranspose1d(in_channels=3,out_channels=3,kernel_size=4,stride=4)
+
+x_up = channel_conv(x_up)
+
+print(x_up.shape)
+
+channel_conv = nn.ConvTranspose1d(in_channels=3,out_channels=3,kernel_size=4,stride=4)
+
+x_up = channel_conv(x_up)
+
+print(x_up.shape)
+
+
 
 # x_down = maxpool(x_added)
 # print(x_down.shape)
